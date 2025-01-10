3cb1cc2327db8e6d0345446da73d31fd29b0aa3d
Cristina Cruceanu <cristinacruceanu@outlook.com>
2025-01-10 12:16:10 +0200

    Github authentication set

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,9 @@
 
-import Hello from "@/app/components/hello";
-
-
 export default function Home() {
-  console.log("What am I?");
+
   return (
-    <>
-      <h1 className="text-3xl">Welcome to Next.js </h1>
-      <Hello />
-    </>
+    <h1 className="text-2xl">
+  HOME
+    </h1>
   );
 }
