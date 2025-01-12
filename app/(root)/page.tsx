3d8c751e8f36259c34bf38282bbad321a8f7c812
Cristina Cruceanu <cristinacruceanu@outlook.com>
2025-01-12 17:37:19 +0200

    hero and search bar implemented

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,23 @@
+import Searchform from "../components/SearchForm";
 
-export default function Home() {
-
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
   return (
-    <h1 className="text-2xl">
-  HOME
-    </h1>
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          From classics to bestsellers, <br /> explore a world of curated books
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Books, Vote on entries, and Get suggestions tailored to your
+          taste
+        </p>
+        <Searchform query={query} />
+      </section>
+    </>
   );
 }
